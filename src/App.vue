a237c483355e4f3739e5916b3f746195fc69d489
Anders Ravn <anders@adriansen.dk>
2022-12-01 12:53:46 +0100

    Add: Scaffolded app structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from "./components/Footer.vue";
+</script>
+
 <template>
+  <Navbar />
   <router-view v-slot="{ Component }">
       <component :is="Component" />
   </router-view>
+  <Footer />
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Bars3Icon } from "@heroicons/vue/24/outline";
+</script>
+
+<template>
+  <header id="navbar">
+    <router-link to="/" class="flex flex-col items-center">
+      <img
+        class="sm:h-24 sm:w-24 h-16 w-16"
+        src="../assets/img/logo.svg"
+        alt="KOLD Icon"
+      />
+      <span
+        class="font-header text-zinc-100 text-lg sm:flex items-center hidden"
+      >
+        <span class="uppercase font-bold">Kold</span><span>Festival</span>
+      </span>
+    </router-link>
+    <button>
+      <Bars3Icon class="h-12 w-12 text-zinc-100" />
+    </button>
+  </header>
+</template>
+
+<style>
+#navbar {
+  @apply fixed z-10 top-0 left-0 right-0 p-4 flex items-center justify-between;
+}
+</style>
