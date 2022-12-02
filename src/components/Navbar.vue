027a393e63402dddea3349996d564e79db18c496
Anders Ravn <anders@adriansen.dk>
2022-12-02 13:04:09 +0100

    Add: Added Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,26 +4,39 @@ import { Bars3Icon } from "@heroicons/vue/24/outline";
 
 <template>
   <header id="navbar">
-    <router-link to="/" class="flex flex-col items-center">
-      <img
-        class="sm:h-24 sm:w-24 h-16 w-16"
-        src="../assets/img/logo.svg"
-        alt="KOLD Icon"
-      />
-      <span
-        class="font-header text-zinc-100 text-lg sm:flex items-center hidden"
+    <nav class="flex items-center justify-between p-4">
+      <router-link to="/" class="flex flex-col items-center">
+        <img class="h-24 w-24 hover:rotate-90 transition-transform duration-300" src="../assets/img/logo.svg" alt="KOLD Icon" />
+      </router-link>
+      <ul
+        class="flex items-center space-x-8 text-lg font-bold font-header text-zinc-100 uppercase"
       >
-        <span class="uppercase font-bold">Kold</span><span>Festival</span>
-      </span>
-    </router-link>
-    <button>
-      <Bars3Icon class="h-12 w-12 text-zinc-100" />
-    </button>
+        <li>
+          <router-link to="/program">Program</router-link>
+        </li>
+        <li>/</li>
+        <router-link to="/faq">F.A.Q</router-link>
+        <li>/</li>
+        <router-link to="/contact">Kontakt</router-link>
+      </ul>
+      <button class="hidden">
+        <Bars3Icon class="h-12 w-12 text-zinc-100" />
+      </button>
+    </nav>
   </header>
 </template>
 
 <style>
 #navbar {
-  @apply fixed z-10 top-0 left-0 right-0 p-4 flex items-center justify-between;
+  @apply fixed z-50 top-0 left-0 right-0;
+}
+
+#navbar ul a {
+  @apply text-zinc-100/75 transition-colors duration-150 hover:text-zinc-100;
+}
+
+#navbar .router-link-active,
+#navbar .router-link-exact-active {
+  @apply text-zinc-100;
 }
 </style>
