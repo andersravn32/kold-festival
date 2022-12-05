235580f2743473d44ca62d2dff4d854dd341c6ad
Anders Ravn <anders@adriansen.dk>
2022-12-05 13:27:33 +0100

    Add: Added accordion.vue

---
+++ src/components/Accordion.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { PlusIcon } from "@heroicons/vue/24/outline";
+
+defineProps({
+  opened: {
+    type: Boolean,
+    default: false,
+  },
+
+  title: {
+    type: String,
+    default: null,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="accordion">
+    <div class="accordion-title" @click="(opened = !opened)">
+      <p>{{ title }}</p>
+      <PlusIcon class="h-6 w-6" />
+    </div>
+
+    <div v-if="opened" class="accordion-content">
+        <slot/>
+    </div>
+  </div>
+</template>
