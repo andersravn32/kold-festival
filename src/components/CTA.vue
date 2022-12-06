84f14c031f1880f34157ba9543a69d004fd1667a
Anders Ravn <anders@adriansen.dk>
2022-12-06 13:34:55 +0100

    Add: Added CTA component

---
+++ src/components/CTA.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+// Import router from composable
+const router = useRouter();
+</script>
+
+<template>
+    <button class="fixed uppercase bottom-0 w-full h-20 bg-green-800 text-center font-bold ">
+        Køb billet
+    </button>
+</template>
