8859842870c75bcab1e2422c7fc830b44d4e6c1f
Anders Ravn <anders@adriansen.dk>
2022-12-07 13:37:51 +0100

    Fix: Fixed CTA.vue

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,12 +1,5 @@
-<script setup>
-import { useRouter } from "vue-router";
-
-// Import router from composable
-const router = useRouter();
-</script>
-
 <template>
-    <button class="fixed uppercase bottom-0 w-full h-20 bg-accent text-center z-50">
+    <a href="https://tix.dk/da/musikkolding/buyingflow/tickets/15815/23040" target="_blank" class="fixed uppercase bottom-0 w-full h-20 bg-accent text-center z-50">
         <p class="font-bold font-header text-zinc-50 text-4xl pt-2 h-full grid place-content-center">Køb billet</p>
-    </button>
+    </a>
 </template>
