95e61ffbb60b9920b302eeeacd44463a44e32c75
Anders Ravn <anders@adriansen.dk>
2022-12-07 13:41:05 +0100

    Add: Fixed CTA.vue structure

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="https://tix.dk/da/musikkolding/buyingflow/tickets/15815/23040" target="_blank" class="fixed uppercase bottom-0 w-full h-20 bg-accent text-center z-1000">
-        <p class="font-bold font-header text-zinc-50 text-4xl pt-2 h-full grid place-content-center">Køb billet</p>
+    <a href="https://tix.dk/da/musikkolding/buyingflow/tickets/15815/23040" target="_blank" class="fixed bottom-0 left-0 right-0 p-4 text-4xl text-center font-header uppercase text-zinc-100 bg-accent font-bold z-[100]">
+        Køb billet
     </a>
 </template>
