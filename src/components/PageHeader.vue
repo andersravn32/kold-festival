cda538eb929475972a714bf0dd6a39cc8aa9e3c8
Anders Ravn <anders@adriansen.dk>
2022-12-08 09:33:03 +0100

    Add: Added page header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    bg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+  <div class="bg-cover bg-center hero-fade" :style="{ 'background-image': `url(${bg})` }">
+    <div class="gradient-backdrop flex flex-col justify-center items-center py-32">
+        <slot />
+    </div>
+  </div>
+</template>
