b6914ab757120517c1cc381566c324c822faa2b8
Anders Ravn <anders@adriansen.dk>
2022-12-08 12:42:58 +0100

    Add: Added base margin bottom to page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-cover bg-center" :style="{ 'background-image': `url(${bg})` }">
+  <div class="bg-cover bg-center mb-4" :style="{ 'background-image': `url(${bg})` }">
     <div class="gradient-backdrop flex flex-col justify-center items-center py-32 gradient-divider">
         <slot />
     </div>
