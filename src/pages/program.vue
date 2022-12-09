2b66f13a3fc9dd198b9dd351e37e786b63f23cd2
Anders Ravn <anders@adriansen.dk>
2022-12-09 14:41:47 +0100

    Add: Added program page filter logic

--- src/pages/program.vue
+++ src/pages/program.vue
@@ -3,6 +3,88 @@ import Tabs from "../components/Tabs.vue";
 import Tab from "../components/Tab.vue";
 import PageHeader from "../components/PageHeader.vue";
 import bg from "../assets/img/bg.jpg";
+
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+// Define router from composable
+const router = useRouter();
+
+// Define loading state
+const loading = ref(false);
+
+// Define global artists variable
+const artists = ref([]);
+
+// Define array for storing number and titles of columns
+const artistCols = ref([]);
+const talkCols = ref([]);
+
+// Define arrays for holding concerts and talks filtered
+const concerts = ref([]);
+const talks = ref([]);
+
+onMounted(async () => {
+  // Update loading state
+  loading.value = true;
+
+  // Fetch artist data
+  const response = await fetch("/src/assets/data.json").then((res) =>
+    res.json()
+  );
+
+  // Map artists as new array with expanded date data
+  artists.value = response.artists.map((artist) => {
+    return {
+      ...artist,
+      dateShort: new Intl.DateTimeFormat("da-DK", {
+        weekday: "long",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      }).format(artist.date * 1000),
+    };
+  });
+
+  // Map each artist as either a concert or a talk
+  concerts.value = artists.value.filter((artist) => {
+    return artist.type == "concert";
+  });
+
+  talks.value = artists.value.filter((artist) => {
+    return artist.type == "talk";
+  });
+
+  // Get number of dates as array
+  concerts.value.forEach((artist) => {
+    if (
+      !artistCols.value.includes(
+        artist.dateShort.substring(0, artist.dateShort.length - 6)
+      )
+    ) {
+      artistCols.value.push(
+        artist.dateShort.substring(0, artist.dateShort.length - 6)
+      );
+    }
+  });
+
+  // Get number of dates as array
+  talks.value.forEach((artist) => {
+    if (
+      !talkCols.value.includes(
+        artist.dateShort.substring(0, artist.dateShort.length - 6)
+      )
+    ) {
+      talkCols.value.push(
+        artist.dateShort.substring(0, artist.dateShort.length - 6)
+      );
+    }
+  });
+
+  // Reset loading state
+  loading.value = false;
+});
 </script>
 
 <template>
@@ -14,87 +96,58 @@ import bg from "../assets/img/bg.jpg";
       </h2>
     </PageHeader>
     <section id="program" class="container mx-auto flex flex-col">
-      <Tabs>
+      <Tabs v-if="artists.length">
         <Tab title="Musik" class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div class="flex flex-col space-y-4 bg-blue-900/75 p-4">
-            <h2 class="text-xl">Torsdag d. 2. Februar</h2>
+          <div
+            v-for="(column, index) in artistCols"
+            :key="index"
+            class="flex flex-col space-y-4 bg-blue-900/75 p-4"
+          >
+            <h2 class="text-xl">{{ column }}</h2>
             <ul class="flex flex-col space-y-2 text-zinc-100">
               <li class="grid grid-cols-4 font-header font-bold">
                 <span class="col-span-2">Navn</span>
                 <span>Sted</span>
                 <span>Tidspunkt</span>
               </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Agnes Hartwich</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Astrid Engberg</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Chopper</span>
-                <span>Godset</span>
-                <span>kl 23:30</span>
-              </li>
-            </ul>
-          </div>
-          <div class="flex flex-col space-y-4 bg-blue-900/75 p-4">
-            <h2 class="text-xl">Fredag d. 3. Februar</h2>
-            <ul class="flex flex-col space-y-2 text-zinc-100">
-              <li class="grid grid-cols-4 font-header font-bold">
-                <span class="col-span-2">Navn</span>
-                <span>Sted</span>
-                <span>Tidspunkt</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Agnes Hartwich</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Astrid Engberg</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Chopper</span>
-                <span>Godset</span>
-                <span>kl 23:30</span>
-              </li>
-            </ul>
-          </div>
-          <div class="flex flex-col space-y-4 bg-blue-900/75 p-4">
-            <h2 class="text-xl">Lørdag d. 4. Februar</h2>
-            <ul class="flex flex-col space-y-2 text-zinc-100">
-              <li class="grid grid-cols-4 font-header font-bold">
-                <span class="col-span-2">Navn</span>
-                <span>Sted</span>
-                <span>Tidspunkt</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Agnes Hartwich</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Astrid Engberg</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Chopper</span>
-                <span>Godset</span>
-                <span>kl 23:30</span>
+              <li
+                v-for="(artist, index) in concerts.filter((artist) => {
+                  return (
+                    artist.dateShort.substring(
+                      0,
+                      artist.dateShort.length - 6
+                    ) == column
+                  );
+                })"
+                :key="index"
+                class="grid grid-cols-4 font-body"
+              >
+                <span class="col-span-2 cursor-pointer underline" @click="router.push(`/artist/${artist.identifier}`)">{{
+                  artist.name
+                }}</span>
+                <span>{{ artist.location }}</span>
+                <span
+                  >kl
+                  {{
+                    artist.dateShort
+                      .substring(
+                        artist.dateShort.length - 6,
+                        artist.dateShort.length
+                      )
+                      .replace(".", ":")
+                  }}</span
+                >
               </li>
             </ul>
           </div>
         </Tab>
 
         <Tab title="Talks" class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div class="flex flex-col space-y-4 bg-blue-900/75 p-4">
+          <div
+            v-for="(column, index) in talkCols"
+            :key="index"
+            class="flex flex-col space-y-4 bg-blue-900/75 p-4"
+          >
             <h2 class="text-xl">Torsdag d. 2. Februar</h2>
             <ul class="flex flex-col space-y-2 text-zinc-100">
               <li class="grid grid-cols-4 font-header font-bold">
@@ -102,30 +155,31 @@ import bg from "../assets/img/bg.jpg";
                 <span>Sted</span>
                 <span>Tidspunkt</span>
               </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Selvforsynende Villahave</span>
-                <span>Godset</span>
-                <span>kl 13:30</span>
-              </li>
-            </ul>
-          </div>
-          <div class="flex flex-col space-y-4 bg-blue-900/75 p-4">
-            <h2 class="text-xl">Fredag d. 3. Februar</h2>
-            <ul class="flex flex-col space-y-2 text-zinc-100">
-              <li class="grid grid-cols-4 font-header font-bold">
-                <span class="col-span-2">Navn</span>
-                <span>Sted</span>
-                <span>Tidspunkt</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Emma Holten</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
-              </li>
-              <li class="grid grid-cols-4 font-body">
-                <span class="col-span-2">Ung Scene</span>
-                <span>Pitstop</span>
-                <span>kl 23:30</span>
+              <li
+                v-for="(artist, index) in talks.filter((artist) => {
+                  return (
+                    artist.dateShort.substring(
+                      0,
+                      artist.dateShort.length - 6
+                    ) == column
+                  );
+                })"
+                :key="index"
+                class="grid grid-cols-4 font-body"
+              >
+                <span class="col-span-2 underline cursor-pointer" @click="router.push(`/artist/${artist.identifier}`)">{{ artist.name }}</span>
+                <span>{{ artist.location }}</span>
+                <span
+                  >kl
+                  {{
+                    artist.dateShort
+                      .substring(
+                        artist.dateShort.length - 6,
+                        artist.dateShort.length
+                      )
+                      .replace(".", ":")
+                  }}</span
+                >
               </li>
             </ul>
           </div>
