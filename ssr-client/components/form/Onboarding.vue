a202f6a83569561a318fa0073331bdfe655e144b
Anders Ravn <anders@adriansen.dk>
2023-01-30 13:27:33 +0100

    Add: Added onboarding logic

--- ssr-client/components/form/Onboarding.vue
+++ ssr-client/components/form/Onboarding.vue
@@ -1,7 +1,26 @@
+<script setup>
+
+// Form input
+const firstName = ref("");
+const lastName = ref("");
+
+const update = () => {
+
+}
+</script>
+
 <template>
-  <form id="form-onboarding">
+  <form id="form-onboarding" @submit.prevent="update">
     <label>Fornavn</label>
+    <input type="text" placeholder="Indtast fornavn">
     <label>Efternavn</label>
+    <input type="text" placeholder="Indtast efternavn">
     <BaseButton> Gem</BaseButton>
   </form>
 </template>
+
+<style>
+#form-onboarding {
+  @apply flex flex-col w-full max-w-sm;
+}
+</style>
--- ssr-client/pages/onboarding.vue
+++ ssr-client/pages/onboarding.vue
@@ -3,28 +3,14 @@ const supabase = useSupabaseClient();
 const user = useSupabaseUser();
 const router = useRouter();
 
-
 // Redirect if user is not signed in
-if (!user.value){
-    router.push("/")
+if (!user.value) {
+  router.push("/");
 }
-
-// Loading state
-const loading = ref(false);
-
-// Update loading state
-loading.value = true;
-
-// Fetch profile data from supabase
-const { data, error } = await supabase.from("profiles").select().eq("id", user.value.id);
-
-// Assign profile data to supabase
-profile.value = data.value[0];
-
 </script>
 
 <template>
-    <section id="page-onboarding">
-        
-    </section>
+  <section id="page-onboarding">
+    <FormOnboarding v-if="user" />
+  </section>
 </template>
