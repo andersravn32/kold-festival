ccb90a8993a0350bd952a7610971fb4a847a88ff
Anders Ravn <anders@adriansen.dk>
2023-01-30 12:49:33 +0100

    Add: Added Signin form component

--- ssr-client/components/form/Signin.vue
+++ ssr-client/components/form/Signin.vue
@@ -1,5 +1,48 @@
+<script setup>
+// Import supabase client
+const supabase = useSupabaseClient();
+
+// Loading state
+const loading = ref(false);
+
+// Email input
+const email = ref("");
+
+// Signin method, using OTP
+const signin = async () => {
+
+    // Ensure that email has input
+    if (!email.value.length){
+        return;
+    }
+
+    // Update loading state
+    loading.value = true;
+
+    // Perform signin request
+    const req = await supabase.auth.signInWithOtp({
+        email: email.value
+    })
+
+    // Handle errors
+    if (req.error){
+        return req.error;
+    }
+
+    // Update loading state
+    loading.value = false;
+};
+</script>
+
 <template>
-    <form @submit.prevent>
-        
-    </form>
+  <form id="form-signin" @submit.prevent="signin">
+    <input v-model="email" type="email" placeholder="Indtast din e-mail" />
+    <BaseButton :loading="loading">Log på </BaseButton>
+  </form>
 </template>
+
+<style>
+#form-signin {
+  @apply flex flex-col w-full max-w-sm;
+}
+</style>
