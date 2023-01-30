932bc22d85611a0e03cdfbd0bdc3e89edefc68f7
Anders Ravn <anders@adriansen.dk>
2023-01-30 16:43:51 +0100

    Add: Added base overlay, change later

---
+++ ssr-client/components/base/Overlay.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="overlay">
+        <div class="relative h-full w-full">
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<style>
+.overlay{
+    @apply fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-50;
+}
+</style>
