0560207302c273b508c87423cb97046d6c3425f6
Anders Ravn <anders@adriansen.dk>
2023-01-30 16:44:04 +0100

    Add: Added based sidebar, change later

--- ssr-client/components/base/Sidebar.vue
+++ ssr-client/components/base/Sidebar.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <aside class="sidebar">
+        
+    </aside>
 </template>
+
+<style>
+.sidebar{
+    @apply fixed right-0 top-0 bottom-0 p-4 bg-midnight w-full max-w-sm;
+}
+</style>
