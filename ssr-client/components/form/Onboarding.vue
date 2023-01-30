7b30ceeaa55a93eeb6423ccedc87dc6fdbe0cd63
Anders Ravn <anders@adriansen.dk>
2023-01-30 13:22:25 +0100

    Add: Added onboarding form and page

---
+++ ssr-client/components/form/Onboarding.vue
@@ -0,0 +1,7 @@
+<template>
+  <form id="form-onboarding">
+    <label>Fornavn</label>
+    <label>Efternavn</label>
+    <BaseButton> Gem</BaseButton>
+  </form>
+</template>
---
+++ ssr-client/pages/onboarding.vue
@@ -0,0 +1,30 @@
+<script setup>
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+const router = useRouter();
+
+
+// Redirect if user is not signed in
+if (!user.value){
+    router.push("/")
+}
+
+// Loading state
+const loading = ref(false);
+
+// Update loading state
+loading.value = true;
+
+// Fetch profile data from supabase
+const { data, error } = await supabase.from("profiles").select().eq("id", user.value.id);
+
+// Assign profile data to supabase
+profile.value = data.value[0];
+
+</script>
+
+<template>
+    <section id="page-onboarding">
+        
+    </section>
+</template>
