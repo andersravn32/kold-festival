f4adf305949f867f4f161b8f27d1ee833b5e8a87
Anders Ravn <anders@adriansen.dk>
2023-01-30 13:26:57 +0100

    Add: Aded user avatar component

---
+++ ssr-client/components/user/Avatar.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+
+  profile: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template></template>
