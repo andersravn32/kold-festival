9a2a56dddc3b52815eb89724b62e54c80a595e80
Anders Ravn <anders@adriansen.dk>
2023-01-30 16:48:51 +0100

    Change: Changed background image of signin page

--- ssr-client/pages/auth/signin.vue
+++ ssr-client/pages/auth/signin.vue
@@ -9,6 +9,6 @@
 <style>
 #page-signin {
   @apply h-screen flex flex-col items-center justify-center bg-cover;
-  background-image: url("https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/bg.jpg");
+  background-image: url("https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/bg2.jpg");
 }
 </style>
