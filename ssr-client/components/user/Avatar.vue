10b218e2fdef69c6707e9543a14a59be7a1f922d
Anders Ravn <anders@adriansen.dk>
2023-01-31 14:18:05 +0100

    Add: Added user avatar component

--- ssr-client/components/user/Avatar.vue
+++ ssr-client/components/user/Avatar.vue
@@ -1,14 +1,31 @@
 <script setup>
 const props = defineProps({
-  user: {
-    type: Object,
-    required: true,
-  },
-
   profile: {
     type: Object,
     required: true,
   },
 });
 </script>
-<template></template>
+<template>
+  <div class="user-avatar">
+    <div v-if="!profile.avatar" class="user-avatar-inner">
+      <span>{{ profile.firstName[0] }}</span>
+      <span>{{ profile.lastName[0] }}</span>
+    </div>
+    <div
+      v-if="profile.avatar"
+      class="user-avatar-inner"
+      :style="{ 'background-image': `url(${profile.avatar})` }"
+    ></div>
+  </div>
+</template>
+
+<style>
+.user-avatar {
+  @apply h-12 w-12 flex-shrink-0;
+}
+
+.user-avatar-inner {
+  @apply h-full w-full rounded-full border border-indigo-600 font-bold font-header flex items-center justify-center text-white bg-center bg-cover;
+}
+</style>
