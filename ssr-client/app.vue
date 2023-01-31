196095830ca643684c505fe63fcac85cd29c5941
Anders Ravn <anders@adriansen.dk>
2023-01-31 13:39:51 +0100

    Add: Added initialization of artists composable

--- ssr-client/app.vue
+++ ssr-client/app.vue
@@ -1,6 +1,11 @@
 <script setup>
 const supabase = useSupabaseClient();
 const user = useSupabaseUser()
+
+// Init artists composable on SSR
+const artists = useArtists();
+await artists.reload();
+
 </script>
 
 <template>
