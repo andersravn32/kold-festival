ad7c94cf14342f957600596b8f95c472f7152479
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-01-31 10:46:38 +0100

    Add: Added navbar component

--- ssr-client/components/base/Navbar.vue
+++ ssr-client/components/base/Navbar.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { Bars3Icon } from '@heroicons/vue/24/outline';
+
+// Navmenu state
+const showNavmenu = ref(false);
+</script>
+
 <template>
-    
+    <header>
+        <nav id="navbar">
+            <NuxtLink to="/">
+                <img class="logo"
+                    src="https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/logo.svg"
+                    alt="KOLD Icon" />
+            </NuxtLink>
+            <ul>
+                <li>
+                    <NuxtLink to="/program">
+                        Program
+                    </NuxtLink>
+                </li>
+                <li>
+                    <button @click="showNavmenu = !showNavmenu" class="text-white">
+                        <Bars3Icon class="h-16 w-16" />
+                    </button>
+                </li>
+            </ul>
+        </nav>
+        <BaseNavmenu v-if="showNavmenu" @close="showNavmenu = !showNavmenu" />
+    </header>
 </template>
+
+<style>
+#navbar {
+    @apply fixed top-0 left-0 right-0 p-4 z-50 flex items-center justify-between bg-gradient-to-b from-blue-900/50 to-transparent;
+}
+
+#navbar .logo {
+    @apply h-24 w-24 hover:rotate-90 transition-transform duration-300;
+}
+
+#navbar ul{
+    @apply text-2xl font-header font-bold text-white/75 flex items-center space-x-8 uppercase;
+}
+</style>
