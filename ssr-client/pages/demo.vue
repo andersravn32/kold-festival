f610d01932e5cad0fc407c5f7202100896278d53
Anders Ravn <anders@adriansen.dk>
2023-02-17 13:07:48 +0100

    Add: Added block testing page

---
+++ ssr-client/pages/demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="demo">
+    <BlockHero
+      :data="{
+        countdown: 2675332000000,
+        bg: {
+          src: 'https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/bg-video.mp4',
+        },
+        logo: {
+          src: 'https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/logo.svg',
+          alt: 'KOLD Logo',
+          spin: true,
+        },
+        title: {
+            html: `<span>KOLD</span><span>Festival</span>` 
+        },
+      }"
+    />
+  </section>
+</template>
