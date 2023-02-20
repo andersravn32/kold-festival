be89d82907b5f87551a463cf9d10258255c986fd
Anders Ravn <anders@adriansen.dk>
2023-02-20 19:59:36 +0100

    Add: Added container block

---
+++ ssr-client/components/block/Container.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+const columns = props.data.html.length;
+</script>
+
+<template>
+  <section class="block-container">
+    <div
+      v-if="data.html"
+      v-for="(col, index) in data.html"
+      :key="index"
+      v-html="col"
+    ></div>
+  </section>
+</template>
+
+<style>
+.block-container {
+  @apply grid items-center;
+  grid-template-columns: repeat(v-bind(columns), 1fr);
+}
+</style>
