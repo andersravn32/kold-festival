6865706fe64e7d364cc311925644e7a18b02772d
LasseK <lkr97km@hotmail.dk>
2023-02-21 17:45:59 +0100

    catch up

--- ssr-client/pages/index.vue
+++ ssr-client/pages/index.vue
@@ -1,14 +1,83 @@
+
+<template>
+  <div id="page-index">
+<!-- Video Hero -->
+   <PageHeaderIndex />
+<!-- Floating GSAP Text -->
+    <div class="overflow-hidden w-full">
+      <div id="artist-banner">
+        <span class="floatingText">KUNSTNERE</span>
+        <span class="floatingText">KUNSTNERE</span>
+        <span>KUNSTNERE</span>
+        <span class="floatingText">KUNSTNERE</span>
+        <span class="floatingText">KUNSTNERE</span>
+      </div>
+    </div> 
+
+<!-- Artis Grid (mangler: se om data virker) -->
+<section class="pb-16" id="artist" v-if="artists.data">
+      <div class="artistGrid">
+        <GridArtist
+          v-for="(artist, index) in artists.data.value"
+          :key="index"
+          :name="artist.name"
+          :subartist="artist.subtitle"
+          :artist-cover="artist.header"
+          @click="router.push(`/artist/${artist.identifier}`)"
+        />
+      </div>
+    </section>
+    
+    
+
+<!-- Ticket Selection -->
+    <section id="tickets">
+      <PricePanel
+        class="price-panel"
+        title="Partout"
+        :price="300"
+        :studentPrice="150"
+      />
+      <PricePanel
+        class="price-panel"
+        title="Fredag"
+        :price="175"
+        :studentPrice="100"
+      />
+      <PricePanel
+        class="price-panel"
+        title="Lørdag"
+        subtitle="(OBS: Gælder ikke Ude af Kontrol)"
+        :price="175"
+        :studentPrice="100"
+      />
+      <div
+        class="price-panel notice"
+      >
+        <InformationCircleIcon class="h-32 w-32" />
+        <h3>Vær opmærksom på..</h3>
+        <p class="font-body text-center">
+          For at opnå student / ung pris skal man være under 25 år eller have et
+          gyldigt studiekort
+        </p>
+      </div>
+    </section>
+  </div>
+
+  <PageAboutSection></PageAboutSection>
+</template>
+
 <script setup>
 /* Meta */
 definePageMeta({
   name: "Hjem",
 });
 /* Imports */
-import { InformationCircleIcon } from "@heroicons/vue/24/solid";
+/* import { InformationCircleIcon } from "@heroicons/vue/24/solid";
 
 // GSAP implementation
 import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger"; */
 
 /* // Supabase
 const supabase = useSupabaseClient();
@@ -82,186 +151,6 @@ ScrollTrigger.create({
  
 });
 </script>
-
-
-
-
-<template>
-  <div id="page-index">
-<!-- Video Hero -->
-   <PageHeaderIndex />
-<!-- Floating GSAP Text -->
-    <div class="overflow-hidden w-full">
-      <div id="artist-banner">
-        <span class="floatingText">KUNSTNERE</span>
-        <span class="floatingText">KUNSTNERE</span>
-        <span>KUNSTNERE</span>
-        <span class="floatingText">KUNSTNERE</span>
-        <span class="floatingText">KUNSTNERE</span>
-      </div>
-    </div> 
-
-<!-- Artis Grid (mangler: se om data virker) -->
-<section class="pb-16" id="artist" v-if="artists.data">
-      <div class="artistGrid">
-        <GridArtist
-          v-for="(artist, index) in artists.data.value"
-          :key="index"
-          :name="artist.name"
-          :subartist="artist.subtitle"
-          :artist-cover="artist.header"
-          @click="router.push(`/artist/${artist.identifier}`)"
-        />
-      </div>
-    </section>
-    
-    
-
-<!-- Ticket Selection -->
-    <section id="tickets">
-      <PricePanel
-        class="price-panel"
-        title="Partout"
-        :price="300"
-        :studentPrice="150"
-      />
-      <PricePanel
-        class="price-panel"
-        title="Fredag"
-        :price="175"
-        :studentPrice="100"
-      />
-      <PricePanel
-        class="price-panel"
-        title="Lørdag"
-        subtitle="(OBS: Gælder ikke Ude af Kontrol)"
-        :price="175"
-        :studentPrice="100"
-      />
-      <div
-        class="price-panel notice"
-      >
-        <InformationCircleIcon class="h-32 w-32" />
-        <h3>Vær opmærksom på..</h3>
-        <p class="font-body text-center">
-          For at opnå student / ung pris skal man være under 25 år eller have et
-          gyldigt studiekort
-        </p>
-      </div>
-    </section>
-  </div>
-
-  <PageAboutSection></PageAboutSection>
-</template>
-
-
-<script setup>
-definePageMeta({
-  name: "Hjem",
-});
-
-/* // Supabase
-const supabase = useSupabaseClient();
-
-const { data: artists } = await useAsyncData('artists', async () => {
-    const { data } = await supabase.from('artists').select();
-
-    return data
-})
- */
-
-const { data } = useArtists();
-console.log(data)
-
-// import router
-const router = useRouter();
-
-const festivalTime = 1675332000000;
-
-const heroTime = ref({
-  days: null,
-  hours: null,
-  minutes: null,
-  seconds: null,
-});
-
-/* const artistsData = ref(
-  artists.filter((artist) => {
-    return artist.type == "concert";
-  })
-); */
-
-const updateTime = () => {
-  // get total seconds between the times
-  var delta = Math.abs(festivalTime - new Date()) / 1000;
-
-  // calculate (and subtract) whole days
-  heroTime.value.days = Math.floor(delta / 86400);
-  delta -= heroTime.value.days * 86400;
-
-  // calculate (and subtract) whole hours
-  heroTime.value.hours = Math.floor(delta / 3600) % 24;
-  delta -= heroTime.value.hours * 3600;
-
-  // calculate (and subtract) whole minutes
-  heroTime.value.minutes = Math.floor(delta / 60) % 60;
-  delta -= heroTime.value.minutes * 60;
-
-  // what's left is seconds
-  heroTime.value.seconds = Math.floor(delta % 60);
-
-  // Credit: Stackoverflow, User: Altinak - https://stackoverflow.com/users/6782/alnitak
-};
-
-setInterval(updateTime, 1000);
-
-// Load gsap when page has mounted
-/* onMounted(async () => {
-
-  // Register gsap plugin
-  gsap.registerPlugin(ScrollTrigger);
-
-  // Floating Banner Effect
-  const banner = gsap.timeline();
-
-  // Enter tickets Effect
-  const tickets = gsap.timeline();
-
-  // Floating Banner Effect 2
-  const talks = gsap.timeline();
-
-  // Position animated banner center
-  banner
-    .from("#artist-banner", { x: "-50%" })
-    .from(".floatingText", { opacity: 1 });
-
-  // Enterings effect tickets
-  tickets.to(".price-panel", {
-    y: 0,
-    opacity: 1,
-    duration: 0.5,
-    ease: "ease-out",
-    stagger: 0.3,
-  });
-
-  // Create scrolltrigger
-  ScrollTrigger.create({
-    animation: banner,
-    trigger: "#artist-banner",
-    scrub: 1,
-    start: "top 90% ",
-    end: "bottom 40%",
-    toggleActions: "restart none none none",
-  });
-
-  ScrollTrigger.create({
-    animation: tickets,
-    trigger: "#tickets",
-    start: "top 70%",
-    end: "bottom 40%",
-    toggleActions: "play none none none",
-  });
-
 
 <style scoped>
 
