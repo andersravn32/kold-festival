1073673d6fc420ecefbaf5728c4d014d0616b6b9
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-02-22 16:23:08 +0100

    Add: Scaffolded container data field form

---
+++ ssr-client/components/form/block/data/Container.vue
@@ -0,0 +1,28 @@
+<script setup>
+const data = ref({
+    html: []
+})
+// On init, push null to array
+// Replace null with correct html markup, which is saved and used in block later
+
+const update = () => {
+    console.log(data.value)
+}
+</script>
+
+<template>
+    <div id="form-block-data-container">
+        <div v-if="!data.html.length" class="h-32 flex items-center flex-col justify-center">
+            <h2>Fandt ingen kolonner</h2>
+            <BaseButton @click.prevent="data.html.push(null)">Tilføj kolonne</BaseButton>
+        </div>
+
+        <div class="flex flex-col space-y-2" v-if="data.html.length">
+            <div class="flex flex-col space-y-2" v-for="(column, index) in data.html" :key="index">
+                <label class="flex items-center justify-between"><span>Kolonne {{ index }} indhold</span><span @click="data.html.splice(index, 1)">Slet</span></label>
+                <textarea class="h-32" v-model="data.html[index]" @change="update">{{ column }}</textarea>
+            </div>
+            <BaseButton @click.prevent="data.html.push(null)">Tilføj kolonne</BaseButton>
+        </div>
+    </div>
+</template>
