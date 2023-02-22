14dbb709e8938bc5be3996ff2ea5c40b48e7cf2f
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-02-22 16:22:48 +0100

    Add: Added form for creating new blocks

---
+++ ssr-client/components/form/block/Create.vue
@@ -0,0 +1,81 @@
+<script setup>
+const supabase = useSupabaseClient();
+
+// Import router from useRouter composable
+const router = useRouter();
+
+// Filter routes, based on hidden routes
+const routes = router.getRoutes().filter((route) => {
+    return !route.meta.hidden
+});
+
+// Loading state
+const loading = ref(false);
+
+// Define base block object
+const block = ref({
+    link: "",
+    link_type: "page",
+    visible: false,
+    data: null,
+    type: "",
+})
+
+// Define component emits
+const emit = defineEmits(['done']);
+
+// Main method for submitting data to database
+const create = async () => {
+    const { data, error } = await supabase.from("blocks").insert(block.value).select();
+
+    // Return done event with either data or error
+    return emit('done', data || error);
+}
+</script>
+
+<template>
+    <div>
+        <form id="form-block-create" @submit.prevent="create">
+            <h1>Opret indhold</h1>
+            <div class="input">
+                <label>Vælg venligst en type af indhold</label>
+                <select v-model="block.type">
+                    <option value="">Vælg venligst</option>
+                    <option disabled>----</option>
+                    <option value="hero">Hero block</option>
+                    <option value="container">Indholds block</option>
+                </select>
+            </div>
+            <div class="input">
+                <label>På hvilken side skal indholdet placeres?</label>
+                <select v-model="block.link">
+                    <option value="">Vælg venligst</option>
+                    <option disabled>----</option>
+                    <option v-for="route in routes" :value="route.path">{{ route.name }}</option>
+                </select>
+            </div>
+            <FormBlockDataHero v-if="block.type == 'hero'" @update="(e) => { block.data = e }" />
+            <FormBlockDataContainer v-if="block.type == 'container'" @update="(e) => { block.data = e }" />
+
+            <div class="checkbox">
+                <input v-model="block.visible" type="checkbox">
+                <label>Offentliggør</label>
+            </div>
+            <BaseButton :loading="loading">Opret indhold</BaseButton>
+        </form>
+    </div>
+</template>
+
+<style>
+#form-block-create {
+    @apply flex flex-col space-y-4;
+}
+
+#form-block-create .input {
+    @apply flex flex-col space-y-2;
+}
+
+#form-block-create .checkbox {
+    @apply flex items-center space-x-2;
+}
+</style>
