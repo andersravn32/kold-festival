331bc7bfdd9d97c597221ba2545546fa8d1b998a
Anders Ravn <anders@adriansen.dk>
2023-02-24 13:34:27 +0100

    Fix: fixed useAccount composable

--- ssr-client/composables/useAccount.js
+++ ssr-client/composables/useAccount.js
@@ -8,11 +8,21 @@ export const useAccount = () => {
     // Assign account user to supabase user
     user.value = useSupabaseUser().value;
 
+    // If no data was loaded, return nothing
+    if (!user.value){
+      return;
+    }
+    
     // Fetch profile data from supabase
     const { data } = await supabase
       .from("profiles")
       .select("*")
       .eq("id", user.value.id);
+
+    // If no data was fetched, return nothing
+    if (!data) {
+      return;
+    }
 
     // Assign account profile data to fetched data
     profile.value = data[0];
