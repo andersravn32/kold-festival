ab123e0273c83d4f9f72fe1b05267252ecab3add
Anders Ravn <anders@adriansen.dk>
2023-02-24 13:34:37 +0100

    Add: Added initial account refresh

--- ssr-client/app.vue
+++ ssr-client/app.vue
@@ -1,6 +1,8 @@
 <script setup>
-const supabase = useSupabaseClient();
-const user = useSupabaseUser()
+const account = useAccount();
+if (!account.profile.value || !account.user.value){
+  await account.update();
+}
 
 // Init artists composable on SSR
 const artists = useArtists();
