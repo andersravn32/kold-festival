c8cb8ed0d5b2b185131b8a5b4d0fdd6b52bd8bc3
Anders Ravn <anders@adriansen.dk>
2023-02-25 14:15:35 +0100

    Add: Added sidebar controls

--- ssr-client/layouts/default.vue
+++ ssr-client/layouts/default.vue
@@ -1,10 +1,11 @@
 <script setup>
 const sidebar = useSidebar();
+const account = useAccount();
 </script>
 
 <template>
   <div>
-    <BaseSidebar v-if="sidebar.show.value" />
+    <BaseSidebar v-if="sidebar.show.value && (account.user.value && account.profile.value)" />
     <slot />
   </div>
 </template>
