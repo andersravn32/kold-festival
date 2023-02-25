9f02c2bf97cd7d1f5ba15ffda050d025667deb04
Anders Ravn <anders@adriansen.dk>
2023-02-25 11:51:02 +0100

    Add: Added overall block content

---
+++ ssr-client/components/block/Content.vue
@@ -0,0 +1,23 @@
+<script setup>
+const router = useRouter();
+const supabase = useSupabaseClient();
+
+const blocks = ref(null);
+
+const { data } = await supabase
+  .from("blocks")
+  .select("*")
+  .eq("link", router.currentRoute.value.path);
+
+blocks.value = data;
+</script>
+
+<template>
+  <div>
+    <div class="block-content" v-for="block in blocks">
+      <BlockHero v-if="block.type == 'hero'" :data="block.data" />
+      <BlockContainer v-if="block.type == 'container'" :data="block.data" />
+      <BlockAccordion v-if="block.type == 'accordion'" :data="block.data" />
+    </div>
+  </div>
+</template>
