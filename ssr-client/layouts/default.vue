fe8a87f61940cd806f5c90d9dcf42d909584e427
Anders Ravn <anders@adriansen.dk>
2023-02-26 13:11:45 +0100

    Add: Added sidebar toggle

--- ssr-client/layouts/default.vue
+++ ssr-client/layouts/default.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { UserIcon } from "@heroicons/vue/24/outline";
 const sidebar = useSidebar();
 const account = useAccount();
 </script>
 
 <template>
   <div>
-    <BaseSidebar v-if="sidebar.show.value && (account.user.value && account.profile.value)" />
+    <BaseSidebar
+      v-if="sidebar.show.value && account.user.value && account.profile.value"
+    />
+    <button v-if="!sidebar.show.value && account.user.value && account.profile.value" @click="sidebar.show.value = true" class="fixed bottom-4 right-4 p-4 bg-indigo-500 rounded-full z-20"><UserIcon class="h-6 w-6 text-white"/></button>
     <slot />
   </div>
 </template>
