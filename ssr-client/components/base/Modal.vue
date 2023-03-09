c071308be6c1073e33a3604663d099b84e728326
Anders Ravn <anders@adriansen.dk>
2023-03-09 13:47:35 +0100

    Add: Added modal component

---
+++ ssr-client/components/base/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+const modal = useModal();
+</script>
+
+<template>
+  <div class="modal">
+    <component v-if="modal.getComponent()" :is="modal.getComponent()" />
+  </div>
+</template>
+
+<style>
+.modal {
+    @apply bg-midnight shadow-md p-4 text-white w-full max-w-md;
+}
+</style>
