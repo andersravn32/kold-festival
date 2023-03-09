cea4177b67e61f61fa3c86f16e18661cd6394cd7
Anders Ravn <anders@adriansen.dk>
2023-03-09 14:47:48 +0100

    Add: Added demo page as index

--- ssr-client/pages/demo.vue
+++ ssr-client/pages/demo.vue
@@ -5,20 +5,8 @@
 </template>
 
 <style>
-#demo .block-header{
-  @apply mb-8;
-}
-
-#demo .block-header h1{
-  @apply text-6xl;
-}
-
-#demo .block-header p{
-  @apply text-xl text-white font-bold uppercase;
-}
-
-#demo .block-accordion {
-  @apply max-w-4xl mx-auto mb-4;
+#demo .blocks{
+  @apply flex flex-col space-y-16;
 }
 
 #demo .block-artist-grid {
