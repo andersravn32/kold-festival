b72983d1864de2119707e2d5b860d35b516ee3b2
Anders Ravn <anders@adriansen.dk>
2023-03-09 13:47:24 +0100

    Add: Added composable for controlling modals

---
+++ ssr-client/composables/useModal.js
@@ -0,0 +1,19 @@
+const component = shallowRef(null);
+const show = ref(false);
+
+export const useModal = () => {
+  const toggle = () => {
+    show.value = !show.value;
+  };
+
+  const setComponent = (comp) => {
+    component.value = comp;
+    return component.value;
+  };
+
+  const getComponent = () => {
+    return component.value;
+  };
+
+  return { toggle, setComponent, getComponent, show, component }
+};
