6ee672dd30bc5b2c2c7d80e294b546b3bc21ee10
Anders Ravn <anders@adriansen.dk>
2023-03-09 13:48:25 +0100

    Add: Added modal support to default layout

--- ssr-client/layouts/default.vue
+++ ssr-client/layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { UserIcon } from "@heroicons/vue/24/outline";
 const sidebar = useSidebar();
+const modal = useModal();
 const account = useAccount();
 </script>
 
@@ -9,7 +10,18 @@ const account = useAccount();
     <BaseSidebar
       v-if="sidebar.show.value && account.user.value && account.profile.value"
     />
-    <button v-if="!sidebar.show.value && account.user.value && account.profile.value" @click="sidebar.show.value = true" class="fixed bottom-4 right-4 p-4 bg-indigo-500 rounded-full z-20"><UserIcon class="h-6 w-6 text-white"/></button>
+
+    <BaseOverlay v-if="modal.show.value">
+      <BaseModal />
+    </BaseOverlay>
+
+    <button
+      v-if="!sidebar.show.value && account.user.value && account.profile.value"
+      @click="sidebar.show.value = true"
+      class="fixed bottom-4 right-4 p-4 bg-indigo-500 rounded-full z-20"
+    >
+      <UserIcon class="h-6 w-6 text-white" />
+    </button>
     <slot />
   </div>
 </template>
