a21e1a82a6323d53723ade3b43a8eb491eafb2ee
Anders Ravn <anders@adriansen.dk>
2023-03-10 16:35:56 +0100

    Refactored: Overlay click event

--- ssr-client/components/base/Overlay.vue
+++ ssr-client/components/base/Overlay.vue
@@ -1,13 +1,27 @@
+<script setup>
+const modal = useModal();
+
+const handleClick = (e) => {
+  if (e.target.className == "overlay-inner") {
+    return modal.toggle();
+  }
+};
+</script>
+
 <template>
-    <div class="overlay">
-        <div class="relative h-full w-full flex items-center justify-center">
-            <slot/>
-        </div>
+  <div class="overlay" @click="handleClick">
+    <div class="overlay-inner">
+      <slot />
     </div>
+  </div>
 </template>
 
 <style>
-.overlay{
-    @apply fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-50;
+.overlay {
+  @apply fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-50;
+}
+
+.overlay-inner {
+  @apply relative h-full w-full flex items-center justify-center;
 }
 </style>
