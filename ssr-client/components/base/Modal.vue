0e46d4ec4aecebb7c72ec28968ffbbc1ca71f5cc
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-03-12 21:14:43 +0100

    Add: Added close button to modals

--- ssr-client/components/base/Modal.vue
+++ ssr-client/components/base/Modal.vue
@@ -1,15 +1,19 @@
 <script setup>
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 const modal = useModal();
 </script>
 
 <template>
   <div class="modal">
-    <component v-if="modal.getComponent()" :is="modal.getComponent()" @close="modal.toggle()"/>
+    <button class="absolute top-4 right-4" @click="modal.toggle()">
+      <XMarkIcon class="h-6 w-6" />
+    </button>
+    <component v-if="modal.getComponent()" :is="modal.getComponent()" @close="modal.toggle()" />
   </div>
 </template>
 
 <style>
 .modal {
-    @apply bg-midnight shadow-md p-4 text-white;
+  @apply bg-midnight shadow-md p-4 text-white relative;
 }
 </style>
