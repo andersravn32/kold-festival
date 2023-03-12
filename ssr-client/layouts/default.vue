42a9405c305b2a4d8454d2f67e4fbaf844414a18
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-03-12 12:21:59 +0100

    Add: Fixed default layout and added footer

--- ssr-client/layouts/default.vue
+++ ssr-client/layouts/default.vue
@@ -6,22 +6,17 @@ const account = useAccount();
 </script>
 
 <template>
-  <div>
-    <BaseSidebar
-      v-if="sidebar.show.value && account.user.value && account.profile.value"
-    />
-
+  <div id="layout-default">
+    <BaseSidebar v-if="sidebar.show.value && account.user.value && account.profile.value" />
     <BaseOverlay v-if="modal.show.value">
       <BaseModal />
     </BaseOverlay>
 
-    <button
-      v-if="!sidebar.show.value && account.user.value && account.profile.value"
-      @click="sidebar.toggle()"
-      class="fixed bottom-4 right-4 p-4 bg-indigo-500 rounded-full z-20"
-    >
+    <button v-if="!sidebar.show.value && account.user.value && account.profile.value" @click="sidebar.toggle()"
+      class="fixed bottom-4 right-4 p-4 bg-indigo-500 rounded-full z-20">
       <UserIcon class="h-6 w-6 text-white" />
     </button>
     <slot />
+    <BaseFooter />
   </div>
 </template>
