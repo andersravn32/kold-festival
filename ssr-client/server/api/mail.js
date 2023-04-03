8bda12c02bac5ba5bc2d6131c33975ae5d722d4c
Anders Ravn <anders@adriansen.dk>
2023-04-03 10:54:18 +0200

    Rewrite: Rewrote backend logic to fit captcha logic

--- ssr-client/server/api/mail.js
+++ ssr-client/server/api/mail.js
@@ -1,27 +1,35 @@
-import { MailtrapClient } from "mailtrap"
+import { MailtrapClient } from "mailtrap";
+import { verify } from "hcaptcha";
 
-export default defineEventHandler(async event => {
+export default defineEventHandler(async (event) => {
+  const { mailtrap_api, mailtrap_token, hcaptcha_secret } = useRuntimeConfig();
 
-    const { mailtrap_api, mailtrap_token, hcaptcha_secret } = useRuntimeConfig()
+  const body = await readBody(event);
 
-    const body = await readBody(event)
-    const TOKEN = mailtrap_token;
-    const ENDPOINT = mailtrap_api;
-
-    const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
-
-    const sender = {
-        email: "mailtrap@koldfestival.dk",
-        name: "Koldfestival formular",
+  const captcha = await verify(hcaptcha_secret, body.captcha);
+  if (!captcha.success) {
+    return {
+      status: 401,
+      msg: "Udfyld venligst captcha",
     };
+  }
 
-    try {
-        client
-            .send({
-                from: sender,
-                to: body.receivers,
-                subject: `Formular fra ${body.firstName}`,
-                html: `
+  const TOKEN = mailtrap_token;
+  const ENDPOINT = mailtrap_api;
+
+  const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+
+  const sender = {
+    email: "mailtrap@koldfestival.dk",
+    name: "KOLD Festival - Forespørgsel",
+  };
+
+  try {
+    const mail = await client.send({
+      from: sender,
+      to: body.receivers,
+      subject: `Formular fra ${body.firstName}`,
+      html: `
                     <!doctype html>
                     <html>
                     <head>
@@ -29,7 +37,7 @@ export default defineEventHandler(async event => {
                     </head>
                     <body style="font-family: sans-serif;">
                         <div style="display: block; margin: auto; max-width: 600px;" class="main">
-                        <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Ny indsendelse fra kontakt formular!</h1>
+                        <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Ny forespørgsel fra kontaktformular:</h1>
                         <p>${body.message}</p>
                         <p><strong>Besvar på følgende email:</strong></p>
                         <p>${body.sender}</p>
@@ -37,16 +45,22 @@ export default defineEventHandler(async event => {
                     </body>
                     </html>
                 `,
-                category: "Kontakt formular",
-                })
+      category: "Kontakt formular",
+    });
 
-        return {
-            status: 200,
-            response: 'Tak for din besked! Vi svarer hurtigst muligt'
-        }
-
-    } catch(err) {
-        console.log(err)
-        return err
+    if (!mail.success) {
+      return {
+        status: 500,
+        msg: "Vi kunne ikke sende din forespørgsel i øjeblikket, prøv venligst igen senere!",
+      };
     }
-})
+
+    return {
+      status: 200,
+      msg: "Tak for din besked! Vi svarer hurtigst muligt",
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+});
