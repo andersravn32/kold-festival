f732558dd3fb67983b02079ab2e983804f16aecc
LasseK <lkr97km@hotmail.dk>
2024-10-14 19:40:19 +0200

    fix: sorting program

--- components/base/Navmenu.vue
+++ components/base/Navmenu.vue
@@ -1,6 +1,9 @@
 <script setup>
 // Define component emits
 defineEmits(["close"]);
+
+const IgIcon = "https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/instafram.svg";
+const FbIcon = "https://ddcpzvjlsezychixcvnh.supabase.co/storage/v1/object/public/public/facebook.svg";
 </script>
 
 <template>
@@ -24,7 +27,19 @@ defineEmits(["close"]);
       <li>
         <NuxtLink class="link" to="/contact" @click="$emit('close')">Kontakt os</NuxtLink>
       </li>
+      <li class="socialsContainer">
+        <div class="socialItemMobile">
+            <a class="socialLinkMobile" href="https://www.instagram.com/koldfestival/" target="_blank"><img class="socialTabIconMobile" :src="IgIcon" /></a>
+        </div>
+        <div class="socialItemMobile">
+            <a class="socialLinkMobile" href="https://www.facebook.com/koldfestival" target="_blank"><img class="socialTabIconMobile" :src="FbIcon" /></a>
+        </div>
+      </li>
     </ul>
+    <!-- <div class=" absolute bottom-4 left-1/2 a">
+      <a class="socialLinkMobile" href="https://www.instagram.com/koldfestival/" target="_blank"><img class="socialTabIconMobile" :src="IgIcon" /></a>
+      <a class="socialLinkMobile" href="https://www.facebook.com/koldfestival" target="_blank"><img class="socialTabIconMobile" :src="FbIcon" /></a>
+    </div> -->
   </div>
 </template>
 
@@ -39,6 +54,27 @@ defineEmits(["close"]);
 
 #navmenu ul li {
   @apply flex justify-center items-center text-4xl xl:text-6xl m-0;
+}
+
+.socialsContainer {
+   @apply flex w-full gap-4;
+}
+
+
+.socialItemMobile {
+    @apply flex md:hidden justify-center items-center transition-all duration-150 bg-blue-600 w-full hover:bg-white rounded-sm;
+}
+
+.socialLinkMobile {
+    @apply h-full w-full py-2 grid place-content-center;
+}
+
+.socialTabIconMobile {
+    @apply h-8 transition-all duration-150;
+}
+
+.socialItemMobile:hover .socialTabIconMobile {
+    filter: invert(36%) sepia(100%) saturate(3918%) hue-rotate(215deg) brightness(94%) contrast(96%);
 }
 
 </style>
